99c021b592f59336cba6a75a5392100e1e3efa89
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-03-07 00:31:04 +0100

    url is changed and removes paywall

--- bot.js
+++ bot.js
@@ -13,74 +13,68 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const grammy_1 = require("grammy");
-//import { Session } from 'grammy/out/platform';
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const apiToken = process.env.TelegramToken;
-const carlosTelegramID = process.env.carlosTelegramID;
-try {
-    // Define a list of authorized users
-    const allowedUserIds = [carlosTelegramID];
-    const bot = new grammy_1.Bot(apiToken);
-    // Define the initial session value.
-    function initial() {
-        return { url: undefined };
-    }
-    // Install session middleware with the initial session value.
-    bot.use((0, grammy_1.session)({ initial }));
-    // Create a middleware function that checks if the user ID is in the allowed list
-    const checkUserMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
-        var _a;
-        const userId = (_a = ctx.from) === null || _a === void 0 ? void 0 : _a.id;
-        if (userId && allowedUserIds.includes(userId.toString())) {
-            // User is allowed, continue to the next middleware
-            yield next();
-        }
-        else {
-            // User is not allowed, send an error message and do not continue
-            yield ctx.reply('Sorry, you are not authorized to use this bot.');
-        }
-    });
-    bot.use(checkUserMiddleware);
-    // Create a middleware function that checks if the user's message contains a URL
-    const checkUrlMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
-        if (ctx && ctx.chat && ctx.message && ctx.message.text) {
-            const urlRegex = /(https?:\/\/[^\s]+)/g;
-            const urlMatches = urlRegex.exec(ctx.message.text);
-            if (urlMatches) {
-                ctx.session.url = urlMatches[0];
-                const options = new grammy_1.InlineKeyboard().text('Option 1', '1').text('Option 2', '2');
-                yield bot.api.sendMessage(ctx.chat.id, 'Select an option:', { reply_markup: options });
-                return;
-            }
-        }
+// Define a list of authorized users
+const allowedUserIds = [process.env.carlosTelegramID];
+const bot = new grammy_1.Bot(process.env.TelegramToken);
+// Define the initial session value.
+function initial() {
+    return { url: undefined };
+}
+// Install session middleware with the initial session value.
+bot.use((0, grammy_1.session)({ initial }));
+// Create a middleware function that checks if the user ID is in the allowed list
+const checkUserMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const userId = (_a = ctx.from) === null || _a === void 0 ? void 0 : _a.id;
+    if (userId && allowedUserIds.includes(userId.toString())) {
+        // User is allowed, continue to the next middleware
         yield next();
-    });
-    // Add the middleware function to the bot
-    bot.use(checkUrlMiddleware);
-    // Handle the callback query for the "1" button
-    bot.on('callback_query:data', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-        if (ctx.session.url) {
-            yield ctx.reply(`You selected Option 1 with URL: ${ctx.session.url}`);
-            delete ctx.session.url; // Remove the URL from the session data
-        }
-        else {
-            yield ctx.reply('No URL was detected.');
-        }
-    }));
-    // Handle the /start command.
-    bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
-    // Handle other messages.
-    bot.on('message', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
-        var _b;
+    }
+    else {
+        // User is not allowed, send an error message and do not continue
+        yield ctx.reply('Sorry, you are not authorized to use this bot.');
+    }
+});
+bot.use(checkUserMiddleware);
+// Create a middleware function that checks if the user's message contains a URL
+const checkUrlMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
+    if (ctx && ctx.chat && ctx.message && ctx.message.text) {
         const urlRegex = /(https?:\/\/[^\s]+)/g;
-        if (!urlRegex.test(((_b = ctx.message) === null || _b === void 0 ? void 0 : _b.text) || '')) {
-            yield ctx.reply('Got another message!');
+        const urlMatches = urlRegex.exec(ctx.message.text);
+        if (urlMatches) {
+            ctx.session.url = urlMatches[0];
+            const options = new grammy_1.InlineKeyboard().text('Option 1', '1').text('Option 2', '2');
+            yield bot.api.sendMessage(ctx.chat.id, 'Select an option:', { reply_markup: options });
+            return;
         }
-    }));
-    // Start the bot.
-    bot.start();
-}
-catch (error) {
-    console.error(error);
-}
+    }
+    yield next();
+});
+// Add the middleware function to the bot
+bot.use(checkUrlMiddleware);
+// Handle the callback query for the "1" button
+bot.on('callback_query:data', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    if (ctx.session.url) {
+        // Modify the URL by appending "https://12ft.io/" to it
+        const modifiedUrl = `https://12ft.io/${ctx.session.url}`;
+        yield ctx.reply(`You selected Option 1 with URL: ${modifiedUrl}`);
+        delete ctx.session.url; // Remove the URL from the session data
+    }
+    else {
+        yield ctx.reply('No URL was detected.');
+    }
+}));
+// Handle the /start command.
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
+// Handle other messages.
+bot.on('message', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    var _b;
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    if (!urlRegex.test(((_b = ctx.message) === null || _b === void 0 ? void 0 : _b.text) || '')) {
+        yield ctx.reply('Got another message!');
+    }
+}));
+// Start the bot.
+bot.start();
--- bot.ts
+++ bot.ts
@@ -1,97 +1,81 @@
-import { Bot , Middleware, Context, Keyboard, InlineKeyboard,session, SessionFlavor } from "grammy";
-//import { Session } from 'grammy/out/platform';
-
+import { Bot, Middleware, Context, InlineKeyboard, session, SessionFlavor } from 'grammy';
 import dotenv from 'dotenv';
-import validUrl from 'valid-url';
 dotenv.config();
 
- 
-
-const apiToken = process.env.TelegramToken;
-const carlosTelegramID= process.env.carlosTelegramID;
-try {
-    // Define the shape of our session.
-    interface SessionData {
-      url?: string;
-    }
-  
-    // Flavor the context type to include sessions.
-    type MyContext = Context & SessionFlavor<SessionData>;
-  
-    // Define a list of authorized users
-    const allowedUserIds = [carlosTelegramID];
-  
-    const bot = new Bot<MyContext>(apiToken!);
-  
 // Define the shape of our session.
 interface SessionData {
-    url?: string;
+  url?: string;
+}
+
+// Flavor the context type to include sessions.
+type MyContext = Context & SessionFlavor<SessionData>;
+
+// Define a list of authorized users
+const allowedUserIds = [process.env.carlosTelegramID];
+
+const bot = new Bot<MyContext>(process.env.TelegramToken!);
+
+// Define the initial session value.
+function initial(): SessionData {
+  return { url: undefined };
+}
+
+// Install session middleware with the initial session value.
+bot.use(session({ initial }));
+
+// Create a middleware function that checks if the user ID is in the allowed list
+const checkUserMiddleware: Middleware<Context> = async (ctx, next) => {
+  const userId = ctx.from?.id;
+  if (userId && allowedUserIds.includes(userId.toString())) {
+    // User is allowed, continue to the next middleware
+    await next();
+  } else {
+    // User is not allowed, send an error message and do not continue
+    await ctx.reply('Sorry, you are not authorized to use this bot.');
   }
-  
-  // Define the initial session value.
-  function initial(): SessionData {
-    return { url: undefined };
+};
+bot.use(checkUserMiddleware);
+
+// Create a middleware function that checks if the user's message contains a URL
+const checkUrlMiddleware = async (ctx: MyContext, next: () => Promise<void>) => {
+  if (ctx && ctx.chat && ctx.message && ctx.message.text) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const urlMatches = urlRegex.exec(ctx.message.text);
+    if (urlMatches) {
+      ctx.session.url = urlMatches[0];
+      const options = new InlineKeyboard().text('Option 1', '1').text('Option 2', '2');
+      await bot.api.sendMessage(ctx.chat.id, 'Select an option:', { reply_markup: options });
+      return;
+    }
   }
-  
-  // Install session middleware with the initial session value.
-  bot.use(session({ initial }));
-  
-  
-    // Create a middleware function that checks if the user ID is in the allowed list
-    const checkUserMiddleware: Middleware<Context> = async (ctx, next) => {
-      const userId = ctx.from?.id;
-      if (userId && allowedUserIds.includes(userId.toString())) {
-        // User is allowed, continue to the next middleware
-        await next();
-      } else {
-        // User is not allowed, send an error message and do not continue
-        await ctx.reply('Sorry, you are not authorized to use this bot.');
-      }
-    };
-    bot.use(checkUserMiddleware);
-  
-    // Create a middleware function that checks if the user's message contains a URL
-    const checkUrlMiddleware = async (ctx: MyContext, next: () => Promise<void>) => {
-      if (ctx && ctx.chat && ctx.message && ctx.message.text) {
-        const urlRegex = /(https?:\/\/[^\s]+)/g;
-        const urlMatches = urlRegex.exec(ctx.message.text);
-        if (urlMatches) {
-          ctx.session.url = urlMatches[0];
-          const options = new InlineKeyboard().text('Option 1', '1').text('Option 2', '2');
-          await bot.api.sendMessage(ctx.chat.id, 'Select an option:', { reply_markup: options });
-          return;
-        }
-      }
-      await next();
-    };
-  
-    // Add the middleware function to the bot
-    bot.use(checkUrlMiddleware);
-  
-    // Handle the callback query for the "1" button
-    bot.on('callback_query:data', async (ctx) => {
-      if (ctx.session.url) {
-        await ctx.reply(`You selected Option 1 with URL: ${ctx.session.url}`);
-        delete ctx.session.url; // Remove the URL from the session data
-      } else {
-        await ctx.reply('No URL was detected.');
-      }
-    });
-  
-    // Handle the /start command.
-    bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
-  
-    // Handle other messages.
-    bot.on('message', async (ctx) => {
-      const urlRegex = /(https?:\/\/[^\s]+)/g;
-      if (!urlRegex.test(ctx.message?.text || '')) {
-        await ctx.reply('Got another message!');
-      }
-    });
-  
-    // Start the bot.
-    bot.start();
-  } catch (error) {
-    console.error(error);
+  await next();
+};
+
+// Add the middleware function to the bot
+bot.use(checkUrlMiddleware);
+
+// Handle the callback query for the "1" button
+bot.on('callback_query:data', async (ctx) => {
+  if (ctx.session.url) {
+    // Modify the URL by appending "https://12ft.io/" to it
+    const modifiedUrl = `https://12ft.io/${ctx.session.url}`;
+    await ctx.reply(`You selected Option 1 with URL: ${modifiedUrl}`);
+    delete ctx.session.url; // Remove the URL from the session data
+  } else {
+    await ctx.reply('No URL was detected.');
   }
-  
+});
+
+// Handle the /start command.
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'));
+
+// Handle other messages.
+bot.on('message', async (ctx) => {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  if (!urlRegex.test(ctx.message?.text || '')) {
+    await ctx.reply('Got another message!');
+  }
+});
+
+// Start the bot.
+bot.start();
