5f5b4cf637f48d42fb07da504c6d95d20719d766
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-03-06 22:41:45 +0100

    implemented user access

--- bot.js
+++ bot.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,10 +16,27 @@ const grammy_1 = require("grammy");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const apiToken = process.env.TelegramToken;
+const carlosTelegramID = process.env.carlosTelegramID;
 // Create an instance of the `Bot` class and pass your authentication token to it.
 const bot = new grammy_1.Bot(apiToken); // <-- put your authentication token between the ""
 // You can now register listeners on your bot object `bot`.
 // grammY will call the listeners when users send messages to your bot.
+// Define a list of authorized users
+const allowedUserIds = [carlosTelegramID];
+// Create a middleware function that checks if the user ID is in the allowed list
+const checkUserMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
+    const userId = (_a = ctx.from) === null || _a === void 0 ? void 0 : _a.id;
+    if (userId && allowedUserIds.includes(userId.toString())) {
+        // User is allowed, continue to the next middleware
+        yield next();
+    }
+    else {
+        // User is not allowed, send an error message and do not continue
+        yield ctx.reply('Sorry, you are not authorized to use this bot.');
+    }
+});
+bot.use(checkUserMiddleware);
 // Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 // Handle other messages.
--- bot.ts
+++ bot.ts
@@ -1,15 +1,33 @@
-import { Bot } from "grammy";
+import { Bot , Middleware, Context} from "grammy";
 import dotenv from 'dotenv'
-dotenv.config()
+dotenv.config();
 
+ 
 
 const apiToken = process.env.TelegramToken;
+const carlosTelegramID= process.env.carlosTelegramID;
 
 // Create an instance of the `Bot` class and pass your authentication token to it.
 const bot = new Bot(apiToken!); // <-- put your authentication token between the ""
 
 // You can now register listeners on your bot object `bot`.
 // grammY will call the listeners when users send messages to your bot.
+
+// Define a list of authorized users
+const allowedUserIds = [carlosTelegramID];
+
+// Create a middleware function that checks if the user ID is in the allowed list
+const checkUserMiddleware: Middleware<Context> = async (ctx, next) => {
+    const userId = ctx.from?.id;
+    if (userId && allowedUserIds.includes(userId.toString())) {
+      // User is allowed, continue to the next middleware
+      await next();
+    } else {
+      // User is not allowed, send an error message and do not continue
+      await ctx.reply('Sorry, you are not authorized to use this bot.');
+    }
+  };
+bot.use(checkUserMiddleware);
 
 // Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
