a422857eafa95cd9e73d9d755026dce78701efc0
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-03-06 22:17:16 +0100

    first commit

---
+++ bot.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const grammy_1 = require("grammy");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const apiToken = process.env.TelegramToken;
+// Create an instance of the `Bot` class and pass your authentication token to it.
+const bot = new grammy_1.Bot(apiToken); // <-- put your authentication token between the ""
+// You can now register listeners on your bot object `bot`.
+// grammY will call the listeners when users send messages to your bot.
+// Handle the /start command.
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+// Handle other messages.
+bot.on("message", (ctx) => ctx.reply("Got another message!"));
+// Now that you specified how to handle messages, you can start your bot.
+// This will connect to the Telegram servers and wait for messages.
+// Start the bot.
+bot.start();
