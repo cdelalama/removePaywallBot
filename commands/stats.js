fdfe73586d1a18449cbe2e7c94186e9f486d1137
Carlos de la Lama-Noriega <cdelalama@gmail.com>
2023-03-09 23:46:05 +0100

    changed mardown to html in stats report

--- commands/stats.js
+++ commands/stats.js
@@ -25,41 +25,11 @@ const statsMiddleware = (ctx, next) => __awaiter(void 0, void 0, void 0, functio
         const result = yield backendless_1.default.Data.of('rpw_users').find(query);
         console.log(`Found ${result.length} users`);
         const message = result.reduce((acc, user) => {
-            acc += `*${user.telegramUsername || 'Unknown User'}*: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
+            acc += `<code>${user.telegramUsername || 'Unknown User'}</code>: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
             return acc;
-        }, '*Number of users and links pasted*\n\n');
-        yield ctx.reply('```' + message + '```', { parse_mode: 'MarkdownV2' });
+        }, '<b>Number of users and links pasted</b>\n\n');
+        yield ctx.reply(message, { parse_mode: 'HTML' });
     }
     yield next();
 });
 exports.statsMiddleware = statsMiddleware;
-/*
-import { Middleware } from 'grammy';
-import Backendless from 'backendless';
-import { MyContext } from '../types/types';
-import { isAdmin } from '../utils/utils';
-import { RpwUser } from '../types/types';
-
-Backendless.initApp(process.env.BackendlessAppId!, process.env.BackendlessApiKey!);
-
-export const statsMiddleware: Middleware<MyContext> = async (ctx: MyContext, next) => {
-  const userId = ctx.from?.id;
-  const isAdminUser = await isAdmin(userId);
-
-  console.log(`User ${userId} is admin: ${isAdminUser}`);
-
-  const query = Backendless.DataQueryBuilder.create();
-  query.setProperties(['telegramId', 'telegramUsername', 'number_calls', 'isAdmin']);
-
-  const result = await Backendless.Data.of<RpwUser>('rpw_users').find(query);
-  console.log(`Found ${result.length} users`);
-  const message = result.reduce((acc, user) => {
-    acc += `*${user.telegramUsername || 'Unknown User'}*: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
-    return acc;
-  }, '*Number of users and links pasted*\n\n');
-
-  await ctx.reply('```' + message + '```', { parse_mode: 'MarkdownV2' });
-
-  await next();
-};
-*/ 
--- commands/stats.ts
+++ commands/stats.ts
@@ -15,43 +15,13 @@ export const statsMiddleware: Middleware<MyContext> = async (ctx: MyContext, nex
       const result = await Backendless.Data.of('rpw_users').find<RpwUser>(query);
       console.log(`Found ${result.length} users`);
       const message = result.reduce((acc, user) => {
-        acc += `*${user.telegramUsername || 'Unknown User'}*: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
+        acc += `<code>${user.telegramUsername || 'Unknown User'}</code>: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
         return acc;
-      }, '*Number of users and links pasted*\n\n');
-  
-      await ctx.reply('```' + message + '```', { parse_mode: 'MarkdownV2' });
+      }, '<b>Number of users and links pasted</b>\n\n');
+      
+      await ctx.reply(message, { parse_mode: 'HTML' });
+
     }
   
     await next();
   };
-  
-  /*
-  import { Middleware } from 'grammy';
-import Backendless from 'backendless';
-import { MyContext } from '../types/types';
-import { isAdmin } from '../utils/utils';
-import { RpwUser } from '../types/types';
-
-Backendless.initApp(process.env.BackendlessAppId!, process.env.BackendlessApiKey!);
-
-export const statsMiddleware: Middleware<MyContext> = async (ctx: MyContext, next) => {
-    const userId = ctx.from?.id;
-    const isAdminUser = await isAdmin(userId);
-  
-    console.log(`User ${userId} is admin: ${isAdminUser}`);
-  
-    const query = Backendless.DataQueryBuilder.create();
-    query.setProperties(['telegramId', 'telegramUsername', 'number_calls', 'isAdmin']);
-  
-    const result = await Backendless.Data.of<RpwUser>('rpw_users').find(query);
-    console.log(`Found ${result.length} users`);
-    const message = result.reduce((acc, user) => {
-      acc += `*${user.telegramUsername || 'Unknown User'}*: ${user.number_calls} (Admin: ${user.isAdmin ? 'Yes' : 'No'})\n`;
-      return acc;
-    }, '*Number of users and links pasted*\n\n');
-  
-    await ctx.reply('```' + message + '```', { parse_mode: 'MarkdownV2' });
-  
-    await next();
-};
-*/
